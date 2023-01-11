abe288475e41cf52e79c9c7c015b6124c33e7760
HuSou <hugo.soulier@edu.ece.fr>
2023-01-11 12:09:52 +0100

    First Push

---
+++ Sudoku.Z3Linq/Z3LinqSolver.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.Z3Linq;
+
+public class Z3LinqSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s.CloneSudoku();
+    }
+
+}
