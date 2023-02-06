68ce8f1d2daabaf35dbf5e3f920974c478db8e7f
Benji0612 <Benjamin.sultan@edu.ece.fr>
2023-02-06 02:17:53 +0100

    version f

---
+++ Sudoku.GraphColoring/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Sudoku.GraphColoring
+{
+    public class Node
+    {
+        public int Row { get; set; } //Ligne
+        public int Col { get; set; } //Colonne
+        public int Value { get; set; } //Valeur (chiffre dans cellule)
+        public int Color { get; set; } //Couleur 
+
+        public Node(int row, int col, int value)
+        {
+            Row = row;
+            Col = col;
+            Value = value;
+            Color = 0;
+        }
+    }
+}
+
