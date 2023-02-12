cd7f154a37e8fa13cd8757025608ec0a0de9c7ad
AlistairMaclellan <65395429+AlistairMaclellan@users.noreply.github.com>
2023-02-12 19:23:29 +0100

    test

---
+++ Sudoku.MethodesHumaines/CellSnapchot.cs
@@ -0,0 +1,22 @@
+﻿namespace Sudoku.MethodesHumaines;
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+
+internal sealed class CellSnapshot
+{
+    public int Value { get; }
+    public ReadOnlyCollection<int> Candidates { get; }
+    public bool IsCulprit { get; }
+    public bool IsSemiCulprit { get; }
+
+    public CellSnapshot(int value, HashSet<int> candidates, bool isCulprit, bool isSemiCulprit)
+    {
+        Value = value;
+        Candidates = new ReadOnlyCollection<int>(candidates.ToArray());
+        IsCulprit = isCulprit;
+        IsSemiCulprit = isSemiCulprit;
+    }
+}
---
+++ Sudoku.MethodesHumaines/Regions.cs
@@ -0,0 +1,36 @@
+﻿using Sudoku.MethodesHumaines;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sudoku.MethodesHumaines;
+
+internal sealed class Region : IEnumerable<Cell>
+{
+    public readonly Cell[] _cells;
+
+    public Cell this[int index] => _cells[index];
+
+    public Region(Cell[] cells)
+    {
+        _cells = (Cell[])cells.Clone();
+    }
+
+    public IEnumerable<Cell> GetCellsWithCandidate(int candidate)
+    {
+        return _cells.Where(c => c.Candidates.Contains(candidate));
+    }
+    public IEnumerable<Cell> GetCellsWithCandidates(params int[] candidates)
+    {
+        return _cells.Where(c => c.Candidates.ContainsAll(candidates));
+    }
+
+    public IEnumerator<Cell> GetEnumerator()
+    {
+        return ((IEnumerable<Cell>)_cells).GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _cells.GetEnumerator();
+    }
+}
