925f65b2b0d606ccf8ed2477baf78851fb641840
Theopinto <56922396+Theopinto@users.noreply.github.com>
2023-02-13 14:54:05 +0100

    supp solver dot net

--- Sudoku.RecuitSimule/RecuitSimuleSolvers.cs
+++ Sudoku.RecuitSimule/RecuitSimuleSolvers.cs
@@ -4,36 +4,7 @@ using System.Resources;
 
 namespace Sudoku.RecuitSimule
 {
-    public class PythonDotNetSolver : PythonSolverBase
-    {
-        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
-        {
-            //System.Diagnostics.Debugger.Break();
-
-            //For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
-            //using (Py.GIL())
-            //{
-            // create a Python scope
-            using (PyModule scope = Py.CreateScope())
-            {
-                // convert the Person object to a PyObject
-                PyObject pySudoku = s.ToPython();
-
-                // create a Python variable "person"
-                scope.Set("sudokud", pySudoku);
-
-                // the person object may now be used in Python
-                string code = Resource1.SelfCallSolver;
-                scope.Exec(code);
-                var result = scope.Get("solution");
-                var toReturn = result.As<Shared.SudokuGrid>();
-                return toReturn;
-            }
-            //}
-
-
-        }
-    }
+ 
     public class RecuitSimulePythonSolver : PythonSolverBase
     {
         public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
