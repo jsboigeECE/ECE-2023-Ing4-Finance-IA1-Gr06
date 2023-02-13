c57c1009fcc80e77228ee5c5f476144e5046079b
Theopinto <56922396+Theopinto@users.noreply.github.com>
2023-02-13 14:42:08 +0100

    solver 2

---
+++ Sudoku.RecuitSimule/RecuitSimuleSolvers.cs
@@ -0,0 +1,129 @@
+﻿using Python.Runtime;
+using Sudoku.Shared;
+using System.Resources;
+
+namespace Sudoku.RecuitSimule
+{
+    public class PythonDotNetSolver : PythonSolverBase
+    {
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+        {
+            //System.Diagnostics.Debugger.Break();
+
+            //For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pySudoku = s.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("sudokud", pySudoku);
+
+                // the person object may now be used in Python
+                string code = Resource1.SelfCallSolver;
+                scope.Exec(code);
+                var result = scope.Get("solution");
+                var toReturn = result.As<Shared.SudokuGrid>();
+                return toReturn;
+            }
+            //}
+
+
+        }
+    }
+    public class RecuitSimulePythonSolver : PythonSolverBase
+    {
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+        {
+
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pyCells = s.Cells.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("sudokud", pyCells);
+
+                string numpyConverter = Resource1.Numpy_convert;
+                scope.Exec(numpyConverter);
+
+                // the person object may now be used in Python
+                string code = Resource1.RecuitSimule;
+                scope.Exec(code);
+                var result = scope.Get("solution");
+                var managedResult = result.As<int[,]>().ToJaggedArray();
+                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+                return new Shared.SudokuGrid() { Cells = managedResult };
+            }
+            //}
+
+        }
+
+
+
+        protected override void InitializePythonComponents()
+        {
+            InstallPipModule("numpy");
+
+            base.InitializePythonComponents();
+        }
+
+    }
+
+
+
+
+    public class SimannealSolverPython : PythonSolverBase
+    {
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+        {
+
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pyCells = s.Cells.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("sudokud", pyCells);
+
+                string numpyConverter = Resource1.Numpy_convert;
+                scope.Exec(numpyConverter);
+
+                // the person object may now be used in Python
+                string code = Resource1.RecuitSimule;
+                scope.Exec(code);
+                var result = scope.Get("solution");
+                var managedResult = result.As<int[,]>().ToJaggedArray();
+                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+                return new Shared.SudokuGrid() { Cells = managedResult };
+            }
+            //}
+
+        }
+
+
+        protected override void InitializePythonComponents()
+        {
+            InstallPipModule("numpy");
+            InstallPipModule("simanneal");
+            base.InitializePythonComponents();
+        }
+
+
+
+
+
+
+
+    }
+
+}
--- Sudoku.RecuitSimule/Resource1.Designer.cs
+++ Sudoku.RecuitSimule/Resource1.Designer.cs
@@ -115,5 +115,35 @@ namespace Sudoku.RecuitSimule {
                 return ResourceManager.GetString("RecuitSimule", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à import copy
+        ///import random
+        ///import numpy as np
+        ///from simanneal import Annealer
+        ///
+        ///sudoku = np.asarray(sudokud)
+        ///
+        ///# from https://neos-guide.org/content/sudoku
+        ///#_ = 0
+        ///#PROBLEM = np.array([
+        ///#    1, _, _,  _, _, 6,  3, _, 8,
+        ///#    _, _, 2,  3, _, _,  _, 9, _,
+        ///#    _, _, _,  _, _, _,  7, 1, 6,
+        ///
+        ///#    7, _, 8,  9, 4, _,  _, _, 2,
+        ///#    _, _, 4,  _, _, _,  9, _, _,
+        ///#    9, _, _,  _, 2, 5,  1, _, 4,
+        ///
+        ///#    6, 2, 9,  _, _, _,  _, _, _,
+        ///#    _, 4, _,  _, _, 7,  6, _, _,
+        ///#    5, _, 7,  6, _, _,  _, _, 3,
+        ///#] [le reste de la chaîne a été tronqué]&quot;;.
+        /// </summary>
+        internal static string Simanneal {
+            get {
+                return ResourceManager.GetString("Simanneal", resourceCulture);
+            }
+        }
     }
 }
